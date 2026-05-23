364a5b2162a9ee153fd9b44099cd2507cbd8c69f
PewDizinho <pewdizinho@gmail.com>
2026-05-22 22:25:17 -0300

    Apk criado

--- elvira/linux/flutter/generated_plugins.cmake
+++ elvira/linux/flutter/generated_plugins.cmake
@@ -3,7 +3,9 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  audioplayers_linux
   file_selector_linux
+  flutter_volume_controller
   sqlite3_flutter_libs
   url_launcher_linux
 )
--- elvira/windows/flutter/generated_plugins.cmake
+++ elvira/windows/flutter/generated_plugins.cmake
@@ -7,6 +7,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   battery_plus
   connectivity_plus
   file_selector_windows
+  flutter_volume_controller
   permission_handler_windows
   sqlite3_flutter_libs
   url_launcher_windows
